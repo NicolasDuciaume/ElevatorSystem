c29e594a9ed5bab372b5b86fc47e30e122e9321f
nazifatanzim <44646521+nazifatanzim@users.noreply.github.com>
2021-02-04 10:43:51 -0500

    Create ElevatorSubsystem class

---
+++ src/ElevatorSubsystem.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+/**
+ * 
+ * @author Nazifa Tanzim, 101074707
+ * @author Chris D'Silva
+ *
+ */
+public class ElevatorSubsystem {
+	private String location;
+	private enum motor{
+		UP, DOWN, STOPPED;
+	};
+	// maybe we'll change this to a list of Button objects
+	private List<Integer> buttons = new ArrayList<Integer>();
+	private List<Boolean> elevatorLamps = new ArrayList<Boolean>();
+	private boolean arrivalLamp;
+	
+	public ElevatorSubsystem() {
+		
+	}
+	
+	private void setArrivalLamp() {
+		
+	}
+	
+	private void setLocation() {
+		
+	}
+	
+	private void setMotor() {
+		
+	}
+
+}
